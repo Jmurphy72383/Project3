59fd71b3bb2faa0e91c29c838d3d778afe531895
JMurphy <murph_iraq@yahoo.com>
2018-12-15 04:13:58 -0500

    file change

--- server.js
+++ server.js
@@ -16,18 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 //Static directory
-app.use(express.static("public"));
-
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/build'));
-  
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
-    });
-  }
+app.use(express.static("public/index.html"));
 
 //Routes
 require("./routes/symbols-api-routes.js")(app);
