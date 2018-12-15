e1fab0d1c29a6a65e6b9244c186b6fb36b5d518c
JMurphy <murph_iraq@yahoo.com>
2018-12-15 12:44:14 -0500

    change path in html routes js file

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
 
   //index route loads home.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 
 };
