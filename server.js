bbd6c5831a3b61d897598bddcc4ba85d796e4262
JMurphy <murph_iraq@yahoo.com>
2018-12-15 12:15:53 -0500

    remove path files

--- server.js
+++ server.js
@@ -20,12 +20,6 @@ app.use(express.static("public"));
 //app.use('/static', express.static(path.join(__dirname, 'client/build')));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-// app.get('/ping', function (req, res) {
-//  return res.send('pong');
-// });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 //Routes
 require("./routes/html-routes.js")(app);
