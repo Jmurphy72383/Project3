d1ae523523cbe3ce61cb33484a64ae9b7f93a210
JMurphy <murph_iraq@yahoo.com>
2018-12-15 04:05:54 -0500

    added code to server.js to troubleshoot non deployment

--- server.js
+++ server.js
@@ -18,6 +18,17 @@ app.use(bodyParser.json());
 //Static directory
 app.use(express.static("public"));
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 //Routes
 require("./routes/symbols-api-routes.js")(app);
 require("./routes/stocks-api-routes.js")(app);
